0c282664b8eaec9efd9ea3a7d87aea03203d213d
Mr. CaT <mrcat323@gmail.com>
2021-09-15 17:41:44 +0500

    homework for students

---
+++ homework.php
@@ -0,0 +1,42 @@
+<?php 
+
+$workers = [
+    [
+        [
+            [
+                'workers' => [
+                    'factory' => [
+                        [
+                            'name' => 'Bill',
+                            'salary' => 400
+                        ],
+                        [
+                            'name' => 'Brixton',
+                            'salary' => 600
+                        ],
+                        [
+                            'name' => 'Shaw',
+                            'salary' => 500
+                        ]
+                        ],
+                    ]
+                [
+                
+                    'office' => [
+                        [
+                            'name' => 'Shelby',
+                            'salary' => 1100
+                        ],
+                        [
+                            'name' => 'Luke',
+                            'salary' => 1100
+                        ],
+                        [
+                            'name' => 'Aaron',
+                            'salary' => 1100
+                        ]]
+                    ]
+            ]
+        ]
+    
+];
