0aec4c45ef3d9ac1af961176150c6c7d14b9982b
Mr. CaT <mrcat323@gmail.com>
2021-09-15 17:30:38 +0500

    multidimentional arrays with for loop which is amazing

--- index.php
+++ index.php
@@ -5,50 +5,62 @@ $workers = [
         [
             [
                 'factory' => [
-                    [
-                        'name' => 'Bill',
-                        'salary' => 400
-                    ],
-                    [
-                        'name' => 'Brixton',
-                        'salary' => 600
-                    ],
-                    [
-                        'name' => 'Shaw',
-                        'salary' => 500
-                    ]
+                        [
+                            'name' => 'Bill',
+                            'salary' => 400
+                        ],
+                        [
+                            'name' => 'Brixton',
+                            'salary' => 600
+                        ],
+                        [
+                            'name' => 'Shaw',
+                            'salary' => 500
+                        ]
                     ],
                     'office' => [
-                        [
-                            'name' => 'Shelby',
-                            'salary' => 1100
-                        ],
-                        [
-                            'name' => 'Luke',
-                            'salary' => 1100
-                        ],
-                        [
-                            'name' => 'Aaron',
-                            'salary' => 1100
-                        ]]
+                            [
+                                'name' => 'Shelby',
+                                'salary' => 1100
+                            ],
+                            [
+                                'name' => 'Luke',
+                                'salary' => 1100
+                            ],
+                            [
+                                'name' => 'Aaron',
+                                'salary' => 1100
+                            ]
+                        ]
                     ]
             ]
         ]
-    ]
 ];
 
-function raise($worker, $name, $raise)
-{
-    if ($worker['name'] == $name) {
-        $worker['salary'] += $raise;
+// $i;
+// $j;
+// $k;
+// $g;
+
+for ($i = 0; $i < sizeof($workers); $i++) {
+    for ($j = 0; $j < sizeof($workers[$i]); $j++) {
+        for ($k = 0; $k < sizeof($workers[$i][$j]); $k++) {
+            echo '<br>     Factory workers: <br>';
+            for($g = 0; $g < sizeof($workers[$i][$j][$k]['factory']); $g++) {
+                echo $workers[$i][$j][$k]['factory'][$g]['name'] . '  =>  ' . $workers[$i][$j][$k]['factory'][$g]['salary'] . '$<br>';
+            }
+
+            echo '<br>     Office workers: <br>';
+            for($g = 0; $g < sizeof($workers[$i][$j][$k]['office']); $g++) {
+                echo $workers[$i][$j][$k]['office'][$g]['name'] . '  =>  ' . $workers[$i][$j][$k]['office'][$g]['salary'] . '$<br>';
+            }
+        }
     }
-
-    return $worker;
 }
 
-foreach ($workers as $worker) {
-    $worker = raise($worker, 'Bill', 200);
-    $worker = raise($worker, 'Shaw', 300);
-    echo $worker['name'] . '    ' . $worker['salary'] . '$<br>';
-}
-
+?>
+<style>
+    body {
+        margin-left: 40px;
+    }
+</style>
